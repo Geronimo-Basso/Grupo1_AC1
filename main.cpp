bb31cd3bff5c2049bad1da55d341755f150c6647
Geronimo Basso <gerobasso01@gmail.com>
2022-09-28 22:11:04 +0200

    Added Book and User entities

--- main.cpp
+++ main.cpp
@@ -1,75 +1,30 @@
 #include <iostream>
+#include "pilagen.h"
 using namespace std;
 
-
-// ----------------------------------------------------Stack-------------------------------------------------------------
-#define SIZE 50
-
-template<typename T> class Stack{
-
+class User{
 public:
-    Stack();
-
-    void push(T elementToAdd);
-
-    T pop();
-
-    bool isFull();
-
-    T topElement();
-
-private:
-    int top;
-    T st[SIZE];
+    int dni;
+    string name;
+    string surname;
 };
 
-template <class T> Stack<T>::Stack() { top = -1; }
+class Book{
+public:
+    string name;
+    string author;
+    string category;
+    bool isTaken;
 
-template <class T> void Stack<T>::push(T elementToAdd)
-{
-    if (isFull()) {
+    Book(const string &name, const string &author, const string &category, bool isTaken) :
+    name(name),
+    author(author),
+    category(category),
+    isTaken(isTaken) {}
+};
 
-        cout << "Invalid operation stack is full \n";
-    }
-    cout << "Element added: " << elementToAdd << endl;
-    top = top + 1;
-    st[top] = elementToAdd;
-}
-
-template <class T> T Stack<T>::pop()
-{
-    T elementToDelete = st[top];
-    top--;
-    return elementToDelete;
-}
-
-template <class T> bool Stack<T>::isFull()
-{
-    if (top == (SIZE - 1))
-        return 1;
-    else
-        return 0;
-}
-
-template <class T> T Stack<T>::topElement()
-{
-    T top_element = st[top];
-    return top_element;
-}
-// ----------------------------------------------------Stack-------------------------------------------------------------
 
 int main() {
-    Stack<int> stack;
-    stack.push(3);
-    stack.push(70);
-    stack.push(42);
-    stack.push(8);
-    stack.push(39);
-    stack.push(37);
-    stack.push(34);
-    stack.push(23);
-    cout << stack.pop() << "\n";
-    cout << stack.topElement()<< "\n";
     return 0;
 }
 
