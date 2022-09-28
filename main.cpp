8035f8591f588e5bba47b648164760f48107c3e2
jorge <jorgecastilla@outlook.com>
2022-09-28 23:10:29 +0200

    Permite crear libros, ejemplares de esos libros, usuarios y asignar libros a esos usuarios
    Ahora tambien permite manegar el historial

--- main.cpp
+++ main.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 #include "pilagen.h"
+#include "Persona.h"
+#include "Libro.h"
+#include "Ejemplar.h"
 using namespace std;
 
-class User{
-public:
-    int dni;
-    string name;
-    string surname;
-};
-
-class Book{
-public:
-    string name;
-    string author;
-    string category;
-    bool isTaken;
-
-    Book(const string &name, const string &author, const string &category, bool isTaken) :
-    name(name),
-    author(author),
-    category(category),
-    isTaken(isTaken) {}
-};
 
 
 int main() {
+    Libro TheGoldenCompass("La Brújula Dorada", 8417092560, "YA");
+    Persona Biblioteca(0, "libre", "libre");
+    Persona Jorge(11223344, "Jorge", "Castilla");
+    Ejemplar TGC1(TheGoldenCompass, 1,Biblioteca);
+    cout << TGC1.getPoseedor().Tostring() << endl;
+    TGC1.Asignar(Jorge);
+    cout << TGC1.getPoseedor().Tostring() << endl;
+    TGC1.Asignar(Biblioteca);
+    Jorge.anadirAlHistorial(TheGoldenCompass);
+    cout << TGC1.getPoseedor().Tostring() << endl;
+    for (int i = 0; i < Jorge.devolverhistorial().size(); ++i) {
+        cout << Jorge.devolverhistorial().at(i);
+    }
     return 0;
 }
 
