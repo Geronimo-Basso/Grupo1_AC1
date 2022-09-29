37d1a7ab7bdcb50f4ae85cac47376640b61b3705
Geronimo Basso <gerobasso01@gmail.com>
2022-09-29 18:30:28 +0200

    Change the name of the classes, addedd all the books and users

--- main.cpp
+++ main.cpp
@@ -1,27 +1,11 @@
 #include <iostream>
-#include "pilagen.h"
-#include "Persona.h"
-#include "Libro.h"
-#include "Ejemplar.h"
+#include "Pila.h"
+#include "User.h"
+#include "Book.h"
 using namespace std;
 
-
-
 int main() {
-    Libro TheGoldenCompass("La Brújula Dorada", 8417092560, "YA");
-    Persona Biblioteca(0, "libre", "libre");
-    Persona Jorge(11223344, "Jorge", "Castilla");
-    Ejemplar TGC1(TheGoldenCompass, 1,Biblioteca);
-    cout << TGC1.getPoseedor().Tostring() << endl;
-    TGC1.Asignar(Jorge);
-    cout << TGC1.getPoseedor().Tostring() << endl;
-    TGC1.Asignar(Biblioteca);
-    Jorge.anadirAlHistorial(TheGoldenCompass);
-    cout << TGC1.getPoseedor().Tostring() << endl;
-    for (int i = 0; i < Jorge.devolverhistorial().size(); ++i) {
-        cout << Jorge.devolverhistorial().at(i);
-    }
-    return 0;
+
 }
 
 
