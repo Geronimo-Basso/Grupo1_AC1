4e150e82477b6573c3bc9f9640912238d507c94a
jorge <jorgecastilla@outlook.com>
2022-09-29 09:12:07 +0200

    Permite crear libros, ejemplares de esos libros, usuarios y asignar libros a esos usuarios
    Ahora tambien permite manegar el historial

---
+++ Menu.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Jorge Castilla on 29/9/22.
+//
+
+#include <iostream>
+#include "Menu.h"
+
+using namespace std;
+Menu::Menu() {
+    char seleccion;
+    while (true)
+    {
+        cout << "Selecione una opcion:" << endl;
+        cin >> seleccion;
+        switch (seleccion){
+            case 'A':
+
+                break;
+
+            case 'B':
+
+                break;
+
+            case 'C':
+
+                break;
+
+            case 'D':
+
+                break;
+        }
+    }
+}
---
+++ Menu.h
@@ -0,0 +1,15 @@
+//
+// Created by Jorge Castilla on 29/9/22.
+//
+
+#ifndef GRUPO1_AC1_MENU_H
+#define GRUPO1_AC1_MENU_H
+using namespace std;
+
+
+class Menu {
+    Menu();
+};
+
+
+#endif //GRUPO1_AC1_MENU_H
