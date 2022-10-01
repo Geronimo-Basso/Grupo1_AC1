b779084208e3ea939fd00964d9688629cb321fa3
Geronimo Basso <gerobasso01@gmail.com>
2022-10-01 13:53:35 +0200

    La primer funcion esta bien, problema con el historial

---
+++ Usuario.cpp
@@ -0,0 +1,24 @@
+#include "Usuario.h"
+#include "pilagen.h"
+
+Usuario::Usuario(string DNI, string nombre, string apellido) {
+    Usuario::DNI=DNI;
+    Usuario::nombre=nombre;
+    Usuario::apellidos=apellido;
+    Usuario::historial=new Pila<Libro>()
+}
+
+string Usuario::getDNI() {
+    return DNI;
+}
+
+string Usuario::Tostring() {
+    return nombre;
+}
+
+void Usuario::anadirAlHistorial(Libro leido) {
+    Usuario::historial.insert(leido);
+}
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
-#include "Pila.h"
-#include "User.h"
-#include "Book.h"
+#include "Usuario.h"
+#include "Libro.h"
+#include "Menu.h"
 using namespace std;
 
 int main() {
-
+    Menu menuPrincipal;
 }
 
 
